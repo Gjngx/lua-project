40f330c1628ad190a9de857487ea1e6626915cac
GiangDang <giangdang2k2@gmail.com>
2026-08-30 14:38:41 +0700

    feat: add drag cursor asset, apply styles, and update social links to open in new tabs

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -611,7 +611,7 @@ export const HomePage = {
 			const worksSvg = $(this.el).find('.home-works-svg')[0];
 			const worksTitle = $(this.el).find('.home-works-main-title')[0];
 			const worksDesc = $(this.el).find('.home-works-main-desc')[0];
-			const worksSvgPadding = viewport.w > 767 ? cvUnit(77, 'rem') : cvUnit(-20, 'rem');
+			const worksSvgPadding = viewport.w > 767 ? cvUnit(68, 'rem') : cvUnit(-20, 'rem');
 
 			const worksTitleTop =
 				worksTitle.getBoundingClientRect().top -
